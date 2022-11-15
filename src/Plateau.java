3a016a6f86086c26f15762b5b4d532d2bc00e846
Christophe <rosse.christophe@gmail.com>
2022-11-15 12:15:34 +0100

    Màj avec la classe plateau

---
+++ src/Plateau.java
@@ -0,0 +1,15 @@
+import java.util.Map;
+
+public class Plateau {
+
+	private Map<Integer, Integer> nombreTires;
+
+	public Map getNombreTires() {
+		return this.nombreTires;
+	}
+
+	public void setNombreTires(Map nombreTires) {
+		this.nombreTires = nombreTires;
+	}
+
+}
