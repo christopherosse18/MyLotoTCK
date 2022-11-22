bdf5b6b26add9a7617b11b4aa95a0ff1ae8f647e
Christophe <rosse.christophe@gmail.com>
2022-11-22 12:23:18 +0100

    Ajout de classes avec leur fonctionnement

--- src/ActionPlateau.java
+++ src/ActionPlateau.java
@@ -1,16 +1,18 @@
 public class ActionPlateau {
 
 	Plateau plateau;
-	/**
-	 * 
-	 * @param nombre
-	 */
 	public void buildPlateau(){
 		plateau = new Plateau();
 	}
-	public void insertionNum(int number) {
-		// TODO - implement ActionPlateau.insertionNum
-		plateau.put(number);
+	public String insertionNum(String nombre) {
+		String message;
+		if(saisieEstBonne(nombre)){
+			int intNombre = Integer.parseInt(nombre);
+			plateau.put(intNombre);
+			return message = "Le nombre "+intNombre+" a été saisi";
+		} else {
+			return message = "Le nombre est négatif ou invalide";
+		}
 	}
 
 	/**
@@ -26,9 +28,33 @@ public class ActionPlateau {
 	 * 
 	 * @param nombre
 	 */
-	public void controleSaisieNum(int nombre) {
-		// TODO - implement ActionPlateau.controleSaisieNum
-		throw new UnsupportedOperationException();
+	public boolean saisieEstBonne(String nombre) {
+		String message;
+		if (!checkNegatif(nombre)){
+			return false;
+		} else if (!checkString(nombre)){
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	public boolean checkNegatif(String nombre){
+		int intNombre = Integer.parseInt(nombre);
+		if (intNombre < 0){
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean checkString(String nombre){
+		if(nombre.chars().allMatch(Character::isDigit)){
+			return false;
+		} else {
+			return false;
+		}
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello worlvcxcvxcxvd!");
+
+        ActionPlateau plateau = new ActionPlateau();
+
+        plateau.buildPlateau();
+
+        System.out.println(plateau.insertionNum("5"));
+
+
     }
 //testkev
 }
